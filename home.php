af38cd41863c573147e3904c21cba2ea7e9689b9
Shin-sibainu <shincode0712@gmail.com>
2021-10-04 21:35:53 +0900

    project start

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+//データベース接続
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Terraria_DB</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<header>
+		<div class="title-and-nav">
+			<div>
+				<h2>Terraria_DB</h2>
+			</div>
+			<nav>
+				<ul>
+					<li>Blog</li>
+					<li>Twitter</li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<div id="container">
+		<!-- 親カテゴリー群 Grid -->
+		<div id="item">A</div>
+		<div id="item">B</div>
+		<div id="item">C</div>
+	</div>
+</body>
+
+</html>
